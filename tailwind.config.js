e5da520da7f49f4820de3e8c6418827053ec2ed3
kasperzvn <kasperhavemus@gmail.com>
2024-04-09 20:00:33 +0200

    modified:   main/index.html modified:   src/input.css modified:   src/output.css modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./main/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        viafont: ["ViaFont", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
