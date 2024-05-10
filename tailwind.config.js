45df83538831e0e4890e09fef80d415131c8ef63
kasper <kasperhavemus@gmail.com>
2024-05-10 22:29:39 +0200

    newest push

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,16 @@ module.exports = {
   content: ["./main/*.{html,js}"],
   theme: {
     extend: {
+      borderWidth: {
+        3: "3px",
+      },
       fontFamily: {
         viafont: ["ViaFontru", "sans-serif"],
         oi: ["Oi", ...defaultTheme.fontFamily.sans]
       },
+      backgroundImage: {
+        "grid": "url(../fonts/grid.png)"
+      }
     },
   },
   plugins: [],
