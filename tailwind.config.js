10962a6e918eb6ffaa78f50139119869878e2735
kasperzvn <kasperhavemus@gmail.com>
2024-05-12 21:12:36 +0200

    Added mine værdier

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,20 @@ module.exports = {
     extend: {
       borderWidth: {
         3: "3px",
+        2.5: "2.5px",
+        1.5: "1.5px",
       },
       fontFamily: {
         viafont: ["ViaFontru", "sans-serif"],
-        syne: ["Syne", ...defaultTheme.fontFamily.sans]
+        syne: ["Syne", ...defaultTheme.fontFamily.sans],
+        anek: ["Anek Latin", ...defaultTheme.fontFamily.sans]
       },
       backgroundImage: {
         "grid": "url(../fonts/grid.png)"
+      },
+      scale: {
+        '103': '1.03',
+        '102': '1.02'
       }
     },
   },
