9dafe896eb48dee51d21e880ffe0dd10513d63e7
kasper <kasperhavemus@gmail.com>
2024-06-08 15:44:54 +0200

    Fixed titles on pictures

--- src/script.js
+++ src/script.js
@@ -71,42 +71,38 @@
 const buttonleft = document.getElementById('carousel_left')
 const buttonright = document.getElementById('carousel_right')
 
-var outofframediv = document.getElementById('outofframecard')
-var farleftdiv = document.getElementById('farleftcard')
-var leftdiv = document.getElementById('leftcard')
-var centerdiv = document.getElementById('centercard')
-var rightdiv = document.getElementById('rightcard')
-var farrightdiv = document.getElementById('farrightcard')
+var outofframediv = document.getElementsByClassName('outofframecard')[0]
+var farleftdiv = document.getElementsByClassName('farleftcard')[0]
+var leftdiv = document.getElementsByClassName('leftcard')[0]
+var centerdiv = document.getElementsByClassName('centercard')[0]
+var rightdiv = document.getElementsByClassName('rightcard')[0]
+var farrightdiv = document.getElementsByClassName('farrightcard')[0]
 
 const frames = {outofframediv, farleftdiv, leftdiv, centerdiv, rightdiv, farrightdiv}
 const framesArray = Object.values(frames);
 
-const picturetoservice = {
-    "../fonts/slider1.jpg": "Service 1",
 
-}
+// for (let i = 0; i < 6; i++) {
+//     const IndexFrame = framesArray[i]
+//     document.addEventListener("DOMContentLoaded", () => {
 
-for (let i = 0; i < 6; i++) {
-    const IndexFrame = framesArray[i]
-    document.addEventListener("DOMContentLoaded", () => {
+//         const observer = new MutationObserver((mutationsList) => {
+//             for (let mutation of mutationsList) {
+//                 if (mutation.type === 'attributes' && mutation.attributeName === 'id') {
+//                     if (mutation.target.id == "centercard") {
+//                         mutation.target.classList.add("scale-[1.03]")
+//                     } else {
+//                         // mutation.target.classList.remove("transform-[105]")
+//                     }
+//                 }
+//             }
+//         });
 
-        const observer = new MutationObserver((mutationsList) => {
-            for (let mutation of mutationsList) {
-                if (mutation.type === 'attributes' && mutation.attributeName === 'id') {
-                    if (mutation.target.id == "centercard") {
-                        mutation.target.classList.add("scale-[1.03]")
-                    } else {
-                        // mutation.target.classList.remove("transform-[105]")
-                    }
-                }
-            }
-        });
+//         const config = { attributes: true, attributeFilter: ['id'] };
+//         observer.observe(IndexFrame, config);
+//     })
 
-        const config = { attributes: true, attributeFilter: ['id'] };
-        observer.observe(IndexFrame, config);
-    })
-
-}
+// }
 
 outofframediv.style.left = "309%"
 farleftdiv.style.left = "-206%"
@@ -123,29 +119,36 @@ buttonleft.onclick = function () {
     }
     deb = true
 
-    outofframediv = document.getElementById('outofframecard')
+    outofframediv = document.getElementsByClassName('outofframecard')[0]
 
     outofframediv.classList.remove("duration-[200ms]")
     outofframediv.classList.remove("ease-out")
     outofframediv.style.left = "-309%"
 
 
-    farleftdiv = document.getElementById('farleftcard')
-    leftdiv = document.getElementById('leftcard')
-    centerdiv = document.getElementById('centercard')
-    rightdiv = document.getElementById('rightcard')
-    farrightdiv = document.getElementById('farrightcard')
+    farleftdiv = document.getElementsByClassName('farleftcard')[0]
+    leftdiv = document.getElementsByClassName('leftcard')[0]
+    centerdiv = document.getElementsByClassName('centercard')[0]
+    rightdiv = document.getElementsByClassName('rightcard')[0]
+    farrightdiv = document.getElementsByClassName('farrightcard')[0]
 
-    centerdiv.classList.remove("scale-[1.03]")
+    // centerdiv.classList.remove("scale-[1.03]")
 
     outofframediv.getElementsByTagName('img')[0].src = farrightdiv.getElementsByTagName('img')[0].src
-    outofframediv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText = ("Service " + farrightdiv.getElementsByTagName('img')[0].src.substring(15,16))
+    // outofframediv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText = ("Service " + farrightdiv.getElementsByTagName('img')[0].src.substring(15,16))
+    outofframediv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText = farrightdiv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText
 
     outofframediv.style.left = "-206%"
     farleftdiv.style.left = "-103%"
     leftdiv.style.left = "0%"
     centerdiv.style.left = "103%"
     rightdiv.style.left = "206%"
+
+    centerdiv.getElementsByTagName('div')[0].classList.remove("opacity-100")
+    centerdiv.getElementsByTagName('div')[0].classList.add("opacity-0")
+
+    leftdiv.getElementsByTagName('div')[0].classList.remove("opacity-0")
+    leftdiv.getElementsByTagName('div')[0].classList.add("opacity-100")
 
     farrightdiv.classList.remove("duration-[200ms]")
     farrightdiv.classList.remove("ease-out")
@@ -157,12 +160,26 @@ buttonleft.onclick = function () {
         outofframediv.classList.add("duration-[200ms]")
         outofframediv.classList.add("ease-out")
 
-        outofframediv.id = "farleftcard"
-        farleftdiv.id = "leftcard"
-        leftdiv.id = "centercard"
-        centerdiv.id = "rightcard"
-        rightdiv.id = "farrightcard"
-        farrightdiv.id = "outofframecard"
+        outofframediv.classList.remove("outofframecard")
+        farleftdiv.classList.remove("farleftcard")
+        leftdiv.classList.remove("leftcard")
+        centerdiv.classList.remove("centercard")
+        rightdiv.classList.remove("rightcard")
+        farrightdiv.classList.remove("farrightcard")
+
+        outofframediv.classList.add("farleftcard")
+        farleftdiv.classList.add("leftcard")
+        leftdiv.classList.add("centercard")
+        centerdiv.classList.add("rightcard")
+        rightdiv.classList.add("farrightcard")
+        farrightdiv.classList.add("outofframecard")
+
+        // outofframediv.id = "farleftcard"
+        // farleftdiv.id = "leftcard"
+        // leftdiv.id = "centercard"
+        // centerdiv.id = "rightcard"
+        // rightdiv.id = "farrightcard"
+        // farrightdiv.id = "outofframecard"
 
         deb = false
     }, 200);
@@ -176,27 +193,35 @@ buttonright.onclick = function () {
     }
     deb = true
 
-    outofframediv = document.getElementById('outofframecard')
+    outofframediv = document.getElementsByClassName('outofframecard')[0]
 
     outofframediv.classList.remove("duration-[200ms]")
     outofframediv.classList.remove("ease-out")
     outofframediv.style.left = "309%"
 
-    farleftdiv = document.getElementById('farleftcard')
-    leftdiv = document.getElementById('leftcard')
-    centerdiv = document.getElementById('centercard')
-    rightdiv = document.getElementById('rightcard')
-    farrightdiv = document.getElementById('farrightcard')
+    farleftdiv = document.getElementsByClassName('farleftcard')[0]
+    leftdiv = document.getElementsByClassName('leftcard')[0]
+    centerdiv = document.getElementsByClassName('centercard')[0]
+    rightdiv = document.getElementsByClassName('rightcard')[0]
+    farrightdiv = document.getElementsByClassName('farrightcard')[0]
 
-    centerdiv.classList.remove("scale-[1.03]")
+    // centerdiv.classList.remove("scale-[1.03]")
 
     outofframediv.getElementsByTagName('img')[0].src = farleftdiv.getElementsByTagName('img')[0].src
+    // outofframediv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText = ("Service " + farleftdiv.getElementsByTagName('img')[0].src.substring(15,16))
+    outofframediv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText = farleftdiv.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerText
 
     outofframediv.style.left = "206%"
     farrightdiv.style.left = "103%"
     rightdiv.style.left = "0%"
     centerdiv.style.left = "-103%"
     leftdiv.style.left = "-206%"
+
+    centerdiv.getElementsByTagName('div')[0].classList.remove("opacity-100")
+    centerdiv.getElementsByTagName('div')[0].classList.add("opacity-0")
+
+    rightdiv.getElementsByTagName('div')[0].classList.remove("opacity-0")
+    rightdiv.getElementsByTagName('div')[0].classList.add("opacity-100")
 
     farleftdiv.classList.remove("duration-[200ms]")
     farleftdiv.classList.remove("ease-out")
@@ -208,12 +233,26 @@ buttonright.onclick = function () {
         outofframediv.classList.add("duration-[200ms]")
         outofframediv.classList.add("ease-out")
 
-        outofframediv.id = "farrightcard"
-        farrightdiv.id = "rightcard"
-        rightdiv.id = "centercard"
-        centerdiv.id = "leftcard"
-        leftdiv.id = "farleftcard"
-        farleftdiv.id = "outofframecard"
+        outofframediv.classList.remove("outofframecard")
+        farleftdiv.classList.remove("farleftcard")
+        leftdiv.classList.remove("leftcard")
+        centerdiv.classList.remove("centercard")
+        rightdiv.classList.remove("rightcard")
+        farrightdiv.classList.remove("farrightcard")
+
+        outofframediv.classList.add("farrightcard")
+        farrightdiv.classList.add("rightcard")
+        rightdiv.classList.add("centercard")
+        centerdiv.classList.add("leftcard")
+        leftdiv.classList.add("farleftcard")
+        farleftdiv.classList.add("outofframecard")
+
+        // outofframediv.id = "farrightcard"
+        // farrightdiv.id = "rightcard"
+        // rightdiv.id = "centercard"
+        // centerdiv.id = "leftcard"
+        // leftdiv.id = "farleftcard"
+        // farleftdiv.id = "outofframecard"
 
         deb = false
     }, 200);
