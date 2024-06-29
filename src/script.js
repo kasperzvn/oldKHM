f24be2135d9e406d5129243fb68b2b161984c440
kasperzvn <kasperhavemus@gmail.com>
2024-06-29 13:55:31 +0200

    fff

--- src/script.js
+++ src/script.js
@@ -214,51 +214,51 @@ const hamburger3 = document.getElementById("hamburger3")
 var menudeb = false
 var menuopen = false
 
-sliderparent.style.display = "none"
-blackoverlay.style.opacity = "0"
-slidermenu.style.right = "-100%"
+// sliderparent.style.display = "none"
+// blackoverlay.style.opacity = "0"
+// slidermenu.style.right = "-100%"
 
-menubutton.onclick = function() {
-    if (menudeb == true) {
-        return
-    }
-    menudeb = true
-    if (menuopen == false) {
-        sliderparent.style.display = "inline"
-        hamburger1.style.transform = "rotateZ(39.75deg)"
-        hamburger3.style.transform = "rotateZ(-39.75deg)"
-        hamburger2.style.opacity = "0"
+// menubutton.onclick = function() {
+//     if (menudeb == true) {
+//         return
+//     }
+//     menudeb = true
+//     if (menuopen == false) {
+//         sliderparent.style.display = "inline"
+//         hamburger1.style.transform = "rotateZ(39.75deg)"
+//         hamburger3.style.transform = "rotateZ(-39.75deg)"
+//         hamburger2.style.opacity = "0"
 
-        setTimeout(() => {
-            blackoverlay.style.opacity = "0.55"
-            slidermenu.style.right = "0%"
-        }, 10);
-        setTimeout(() => {
-            menuopen = true
-            menudeb = false
-        }, 250);
-    } else if (menuopen == true) {
-        console.log("closing")
-        hamburger1.style.transform = "rotateZ(0deg)"
-        hamburger3.style.transform = "rotateZ(0deg)"
-        hamburger2.style.opacity = "1"
+//         setTimeout(() => {
+//             blackoverlay.style.opacity = "0.55"
+//             slidermenu.style.right = "0%"
+//         }, 10);
+//         setTimeout(() => {
+//             menuopen = true
+//             menudeb = false
+//         }, 250);
+//     } else if (menuopen == true) {
+//         console.log("closing")
+//         hamburger1.style.transform = "rotateZ(0deg)"
+//         hamburger3.style.transform = "rotateZ(0deg)"
+//         hamburger2.style.opacity = "1"
 
-        setTimeout(() => {
-            blackoverlay.style.opacity = "0"
-            slidermenu.style.right = "-100%"
-        }, 10);
-        setTimeout(() => {
-            sliderparent.style.display = "none"
-            menuopen = false
-            menudeb = false
-        }, 250);
-    }
-}
+//         setTimeout(() => {
+//             blackoverlay.style.opacity = "0"
+//             slidermenu.style.right = "-100%"
+//         }, 10);
+//         setTimeout(() => {
+//             sliderparent.style.display = "none"
+//             menuopen = false
+//             menudeb = false
+//         }, 250);
+//     }
+// }
 
 const intervalId = setInterval(() => {
     if (!(window.matchMedia("(min-width: 768px)").matches)) {
         if (performance.now() - timestamp >= 5000) {
-            leftbuttonfunction();
+            rightbuttonfunction();
             timestamp = performance.now();
         }
     }
